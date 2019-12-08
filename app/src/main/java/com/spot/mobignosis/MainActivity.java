8779a8d940cb787c6dd74892ebec666bcd9720b7
abdul <abdul.tanveer@gmail.com>
2019-12-08 11:54:45 +0530

    Initial commit

---
+++ app/src/main/java/com/spot/mobignosis/MainActivity.java
@@ -0,0 +1,23 @@
+package com.spot.mobignosis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void handleClick(View view) {
+        Toast.makeText(this, "mobignosis", Toast.LENGTH_SHORT).show();
+        Intent homeIntent = new Intent(MainActivity.this,HomeActivity.class);
+        startActivity(homeIntent);
+    }
+}
