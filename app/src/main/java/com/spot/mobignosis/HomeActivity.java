674a3d08041eaa2925af23a22dff55cd7f667f7e
abdul <abdul.tanveer@gmail.com>
2019-12-08 12:47:22 +0530

    log and logcat

--- app/src/main/java/com/spot/mobignosis/HomeActivity.java
+++ app/src/main/java/com/spot/mobignosis/HomeActivity.java
@@ -3,15 +3,20 @@ package com.spot.mobignosis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+    public static String TAG = HomeActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-       String name = getIntent().getExtras().getString("myname");
+        Log.e(TAG,"homeactivity started");
+
+        String name = getIntent().getExtras().getString(MainActivity.KEY);
         TextView naTextView = findViewById(R.id.textViewResult);
         naTextView.setText(name);
     }
--- app/src/main/java/com/spot/mobignosis/MainActivity.java
+++ app/src/main/java/com/spot/mobignosis/MainActivity.java
@@ -4,26 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static String KEY = "myname";
+    public static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG,"mainactivity was created");
     }
 
     public void handleClick(View view) {
        // Toast.makeText(this, "mobignosis", Toast.LENGTH_SHORT).show();
+        Log.e(TAG,"button clicked");
+
         EditText nameEditText;
         nameEditText = findViewById(R.id.editTextName);
         String name = "";
         name = nameEditText.getText().toString();
         Intent homeIntent = new Intent(MainActivity.this,HomeActivity.class);
-        homeIntent.putExtra("myname",name);
+        homeIntent.putExtra(KEY,name);
+        Log.w(TAG,"starting home activity");
+
         startActivity(homeIntent);
     }
 }
+
