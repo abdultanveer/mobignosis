708d8ac0a2193d2e52f5bd799695bd1d93f11ea4
abdul <abdul.tanveer@gmail.com>
2019-12-08 12:57:02 +0530

    explicit and implicit intents

--- app/src/main/java/com/spot/mobignosis/MainActivity.java
+++ app/src/main/java/com/spot/mobignosis/MainActivity.java
@@ -3,6 +3,7 @@ package com.spot.mobignosis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -22,13 +23,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void handleClick(View view) {
        // Toast.makeText(this, "mobignosis", Toast.LENGTH_SHORT).show();
+
+        switch (view.getId()){
+            case R.id.buttonHome:
+                launchHomeActivity();
+                break;
+            case R.id.buttondialer:
+                launchDialerActivity();
+                break;
+        }
+
+    }
+
+    private void launchDialerActivity() {
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:123456789"));//implicit intent
+        startActivity(dialIntent);
+    }
+
+    private void launchHomeActivity() {
         Log.e(TAG,"button clicked");
 
         EditText nameEditText;
         nameEditText = findViewById(R.id.editTextName);
         String name = "";
         name = nameEditText.getText().toString();
-        Intent homeIntent = new Intent(MainActivity.this,HomeActivity.class);
+        Intent homeIntent = new Intent(MainActivity.this,HomeActivity.class);//explicit intent
         homeIntent.putExtra(KEY,name);
         Log.w(TAG,"starting home activity");
 
